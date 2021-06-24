f7b3b73f1d264af7269120206bd7a9cd6c9e1feb
Thounaojam-Bafana <76650032+Thounaojam-Bafana@users.noreply.github.com>
2021-06-24 13:36:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+ var leftWristX,rightWristX,difference;
 function setup(){
     video = createCapture(VIDEO);
     video.size(550,500);
@@ -8,14 +9,25 @@ function setup(){
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
 }
+
 function draw(){
     background("#ccccff");
+    textSize(difference);
+    fill("black");
+    text("Bafana", 100,200);
 }
+
 function modelLoaded(){
     console.log("Posenet is initialised!");
 }
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        console.log("Left Wrist X = " + leftWristX);
+        rightWristX = results[0].pose.rightWrist.x;
+        console.log("Right Wrist X = "+ rightWristX);
+        difference = floor(leftWristX - rightWristX);
+        console.log("difference = "+ difference);
     }
 }
